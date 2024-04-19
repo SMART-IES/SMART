1aeca1c05a2b834cdeddc907d50ad32f44dd9a6e
florentin koch <florentin.koch@insa-lyon.fr>
2024-04-19 23:39:16 +0200

    scrap all elements of a webpage + able to highlight some of them

--- app/js/content.js
+++ app/js/content.js
@@ -1,5 +1,4 @@
-
-
+//---Test : count function---// 
 function scrape() {
     if(document.getElementById("count_number")) {
         return;
@@ -25,9 +24,35 @@ function sendNumber(count) {
     chrome.runtime.sendMessage({ message: "update_number", count: count });
 }
 
+//---Scrap all text elements---// 
+function getAllTextElements() {
+  let textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, div, li, td, a');
+  let allTexts = [];
+
+  textElements.forEach(element => {
+      let textContent = element.textContent.trim();
+      if (textContent.length > 0) {
+          allTexts.push(textContent);
+      }
+  });
+
+  return allTexts;
+}
+
+function highlightTextElements(allTexts) {
+  allTexts.forEach(text => {
+    let elements = document.querySelectorAll('h2');
+    elements.forEach(element => {
+      element.style.backgroundColor = 'yellow';
+    });
+  });
+}
+
 chrome.runtime.onMessage.addListener((request) => {
     if (request.message === "analyze_site") {
       scrape();
+      let allTextElements = getAllTextElements();
+      highlightTextElements(allTextElements);
     } else if (request.message === "open_popup") {
       let element = document.getElementById("count_number");
       if (element) {
